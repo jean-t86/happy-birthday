19409daadeac5c2db8dc6b8d6dbaab378d02a097
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-02 11:24:42 +1000

    Update GreetingText to display happy birthday text and from text

--- app/src/main/java/me/tadebois/happybirthday/MainActivity.kt
+++ app/src/main/java/me/tadebois/happybirthday/MainActivity.kt
@@ -3,13 +3,19 @@ package me.tadebois.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import me.tadebois.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -19,11 +25,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
+                // The surface container is mandatory/required to be the single
+                // child of the HappyBirthdayTheme composable.
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingText(message = "Happy Birthday Sam!", from = "From John")
                 }
             }
         }
@@ -32,15 +40,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = modifier
+                .padding(16.dp)
+                .align(Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
